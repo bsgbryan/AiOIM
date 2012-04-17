d73316c8020950b8899058c0dda406db801df043
Bryan Maynard <bsgbryan@gmail.com>
2012-04-16 23:25:01 -0500

    Trying to figure twitter oath out

--- web.js
+++ web.js
@@ -28,7 +28,7 @@ app.get('/', function(req, res) {
   res.render('example', { layout : false })
 })
 
-app.get('/oauth_callback', function(req, res) {
+app.get('/twitter/signin', function (req, res) {
   consumer().getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
     if (error) {
       res.send("Error getting OAuth request token : " + sys.inspect(error), 500);
@@ -36,6 +36,26 @@ app.get('/oauth_callback', function(req, res) {
       req.session.oauthRequestToken = oauthToken;
       req.session.oauthRequestTokenSecret = oauthTokenSecret;
       res.redirect("https://api.twitter.com/1/oauth/authorize?oauth_token="+req.session.oauthRequestToken);      
+    }
+  })
+})
+
+app.get('/twitter/callback', function(req, res) {
+  consumer().getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
+    if (error) {
+      res.send("Error getting OAuth access token : " + sys.inspect(error) + "["+oauthAccessToken+"]"+ "["+oauthAccessTokenSecret+"]"+ "["+sys.inspect(results)+"]", 500)
+    } else {
+      req.session.oauthAccessToken = oauthAccessToken
+      req.session.oauthAccessTokenSecret = oauthAccessTokenSecret
+      // Right here is where we would write out some nice user stuff
+      consumer.get("http://twitter.com/account/verify_credentials.json", req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, function (error, data, response) {
+        if (error) {
+          res.send("Error getting twitter screen name : " + sys.inspect(error), 500)
+        } else {
+          req.session.twitterScreenName = data["screen_name"]
+          res.send('You are signed in: ' + req.session.twitterScreenName)
+        }  
+      });  
     }
   });
 })
