de0933d651a19fe09aa39928bdfe185582185677
Bryan Maynard <bsgbryan@gmail.com>
2012-04-16 23:16:44 -0500

    Trying to figure twitter oath out

--- web.js
+++ web.js
@@ -2,6 +2,20 @@ var express = require('express')
 
 var app = express.createServer(express.logger())
 
+var key = "YOURTWITTERCONSUMERKEY";
+var secret = "YOURTWITTERCONSUMERSECRET";
+
+function consumer() {
+  return new oauth.OAuth(
+    "https://api.twitter.com/1/oauth/request_token", 
+    "https://api.twitter.com/1/oauth/access_token", 
+    "myQEbQTOpgo3Ql9ylTtg", 
+    "du2TORpsR39s0ovo1q0EFmicUlIh2gseufVjnQn5o", 
+    "1.0", 
+    "http://falling-samurai-7438.herokuapp.com/oauth_callback", 
+    "HMAC-SHA1");   
+}
+
 app.configure(function() {
   app.use(express.static(__dirname + '/app'))
 
@@ -15,7 +29,15 @@ app.get('/', function(req, res) {
 })
 
 app.get('/oauth_callback', function(req, res) {
-  res.redirect('https://api.twitter.com/1/oauth/authenticate?oauth_token=' + req.param('oauth_token'))
+  consumer().getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
+    if (error) {
+      res.send("Error getting OAuth request token : " + sys.inspect(error), 500);
+    } else {  
+      req.session.oauthRequestToken = oauthToken;
+      req.session.oauthRequestTokenSecret = oauthTokenSecret;
+      res.redirect("https://api.twitter.com/1/oauth/authorize?oauth_token="+req.session.oauthRequestToken);      
+    }
+  });
 })
 
 // The port number is passed in via Heroku
