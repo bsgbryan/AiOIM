aff322843eb5f46cfe503043f848bb1907afee5e
Bryan Maynard <bsgbryan@gmail.com>
2012-06-03 16:35:04 -0500

    Experimenting with how to send user profile to final_destination

--- app/javascript/loqui.js
+++ app/javascript/loqui.js
@@ -1,19 +1,29 @@
-$(document).ready(function () {
-  var loqui = $('#loqui'),
-      url   = 'http://falling-samurai-7438.herokuapp.com/twitter/signin?final_destination=' + window.location,
-      profile
+(function ($) {
+  $.loqui = function (client, element) {
+    var container = typeof element === 'undefined' ? 'loqui' : element,
+        loqui = $('#' + container)
 
-  if (loqui.length === 0)
-    loqui = $('body').
-      append('<div id="loqui"></div>').
-      find('#loqui')
+    if (loqui.length === 0)
+      loqui = $('body').
+        append('<div id="' + container + '"></div>').
+        find('#' + container)
 
-  if (location.hash.indexOf('#twitter_profile=') === 0) {
-    $.cookie('twitter_profile', location.hash.substring(17))
-    location.hash = ''
+    if ($.cookie('twitter_profile') === null) {
+      var url = 'http://falling-samurai-7438.herokuapp.com/twitter/signin?final_destination=' + client,
+          profile
+
+      if (location.hash.indexOf('#twitter_profile=') === 0) {
+        $.cookie('twitter_profile', location.hash.substring(17))
+        location.hash = ''
+      }
+
+      loqui.append('<a href="' + url + '">sign in</button>')
+    } else {
+      var profile = JSON.parse($.cookie('twitter_profile'))
+
+      console.log('logged in as', profile.screen_name)
+    }
   }
+})(jQuery)
 
-  console.log(JSON.parse($.cookie('twitter_profile')))
-
-  loqui.append('<a href="' + url + '">sign in</button>')
-})
+$.loqui('loqui', window.location)
