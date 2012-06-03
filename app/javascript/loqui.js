92f9c1cd0a52ee6eda5ac36cb95c1825e9cbd3a3
Bryan Maynard <bsgbryan@gmail.com>
2012-06-03 16:04:16 -0500

    Decoding uri component now

--- app/javascript/loqui.js
+++ app/javascript/loqui.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
   var loqui = $('#loqui'),
       url   = 'http://falling-samurai-7438.herokuapp.com/twitter/signin?final_destination=' + window.location,
       profile
-      
+
   if (loqui.length === 0)
     loqui = $('body').
       append('<div id="loqui"></div>').
       find('#loqui')
 
   if (location.search.indexOf('?twitter_profile=') === 0)
-    profile = JSON.parse(location.search.substring(17))
+    profile = JSON.parse(decodeURIComponent(location.search.substring(17)))
 
   console.log(profile)
 
