06f8c2af450d3863136d92acc1e4123f17d6dcc7
Bryan Maynard <bsgbryan@gmail.com>
2012-06-03 14:45:56 -0500

    Added some logging

--- web.js
+++ web.js
@@ -5,13 +5,17 @@ var express = require('express'),
 
 var app = express.createServer(express.logger()),
     oa  = new OAuth(
-  'https://api.twitter.com/oauth/request_token', 
-  'https://api.twitter.com/oauth/access_token', 
-  'myQEbQTOpgo3Ql9ylTtg', 
-  'du2TORpsR39s0ovo1q0EFmicUlIh2gseufVjnQn5o', 
-  '1.0', 
-  'http://falling-samurai-7438.herokuapp.com/twitter/callback', 
-  'HMAC-SHA1')
+      'https://api.twitter.com/oauth/request_token', 
+      'https://api.twitter.com/oauth/access_token', 
+      'myQEbQTOpgo3Ql9ylTtg', 
+      'du2TORpsR39s0ovo1q0EFmicUlIh2gseufVjnQn5o', 
+      '1.0', 
+      'http://falling-samurai-7438.herokuapp.com/twitter/callback', 
+      'HMAC-SHA1')
+
+// Twitter urls
+var creds = 'http://twitter.com/account/verify_credentials.json',
+    auth  = 'https://api.twitter.com/oauth/authorize?oauth_token='
 
 app.configure(function() {
   app.use(express.static(__dirname + '/app'))
@@ -28,37 +32,34 @@ app.get('/', function(req, res) {
 })
 
 app.get('/twitter/signin', function (req, res) {
-  oa.getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
-    if (error) {
-      console.log('error', error)
-      res.send('Error getting OAuth request token : ' + error, 500);
-    } else {  
-      req.session.oauthRequestToken = oauthToken;
-      req.session.oauthRequestTokenSecret = oauthTokenSecret;
-      res.redirect('https://api.twitter.com/oauth/authorize?oauth_token=' + req.session.oauthRequestToken);      
+  oa.getOAuthRequestToken(function(error, token, secret, results) {
+
+    if (error) res.send(error, 500)
+    else {  
+      req.session.token  = token
+      req.session.secret = secret
+
+      res.redirect(auth + token)    
     }
   })
 })
 
 app.get('/twitter/callback', function(req, res) {
-  oa.getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
-    if (error) {
-      res.send('Error getting OAuth access token : ' + sys.inspect(error) + '['+oauthAccessToken+']'+ '['+oauthAccessTokenSecret+']'+ '['+sys.inspect(results)+']', 500)
-    } else {
-      req.session.oauthAccessToken = oauthAccessToken
-      req.session.oauthAccessTokenSecret = oauthAccessTokenSecret
-      // Right here is where we would write out some nice user stuff
-      oa.getProtectedResource('http://twitter.com/account/verify_credentials.json', 'GET', req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, function (error, data, response) {
-        if (error) {
-          res.send('Error getting twitter screen name : ' + sys.inspect(error), 500)
-        } else {
-          req.session.twitterScreenName = data['screen_name']
-          res.send('data: ' + JSON.parse(data).screen_name)
-          // console.log('response', )
-        }  
-      });  
-    }
-  });
+  
+  oa.getOAuthAccessToken(
+    req.session.token, 
+    req.session.secret, 
+    req.query.oauth_verifier,
+
+    function(error, token, secret, results) {
+      if (error)
+        res.send(error, 500)
+      else
+        oa.getProtectedResource(creds, 'GET', token, secret, function (error, data, response) {
+          if (error) res.send(error, 500)
+          else res.send(data)  
+        })
+  })
 })
 
 // The port number is passed in via Heroku
