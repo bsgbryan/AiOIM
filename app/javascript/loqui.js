1f1766bd50d3ddd92945808f51c6e7227e6631e6
Bryan Maynard <bsgbryan@gmail.com>
2012-06-03 16:00:13 -0500

    Parse the user's twitter profile into a json object

--- app/javascript/loqui.js
+++ app/javascript/loqui.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
   var loqui = $('#loqui'),
       url   = 'http://falling-samurai-7438.herokuapp.com/twitter/signin?final_destination=' + window.location
-
+      profile
   if (loqui.length === 0)
     loqui = $('body').
       append('<div id="loqui"></div>').
       find('#loqui')
 
+  if (location.search.indexOf('?twitter_profile=') === 0)
+    profile = JSON.parse(location.search.substring(17))
+
+  console.log(profile)
+  
   loqui.append('<a href="' + url + '">sign in</button>')
 })
