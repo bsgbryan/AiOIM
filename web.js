c618bf4b67ecc97270fc867090ad8e914bb4a0c2
Bryan Maynard <bsgbryan@gmail.com>
2012-06-03 14:04:30 -0500

    Trying things out

--- web.js
+++ web.js
@@ -1,21 +1,22 @@
 var express = require('express'),
-    oauth   = require('node-oauth')
+    OAuth   = require('oauth').OAuth,
+    sha1    = require('sha1'),
+    secret  = sha1.hash(new Date().getTime())
 
-var app = express.createServer(express.logger())
-
-function consumer() {
-  return new oauth.OAuth(
-    "https://api.twitter.com/1/oauth/request_token", 
-    "https://api.twitter.com/1/oauth/access_token", 
-    "myQEbQTOpgo3Ql9ylTtg", 
-    "du2TORpsR39s0ovo1q0EFmicUlIh2gseufVjnQn5o", 
-    "1.0", 
-    "http://falling-samurai-7438.herokuapp.com/oauth_callback", 
-    "HMAC-SHA1");   
-}
+var app = express.createServer(express.logger()),
+    oa  = new OAuth(
+  "https://api.twitter.com/oauth/request_token", 
+  "https://api.twitter.com/oauth/access_token", 
+  "myQEbQTOpgo3Ql9ylTtg", 
+  "du2TORpsR39s0ovo1q0EFmicUlIh2gseufVjnQn5o", 
+  "1.0", 
+  "http://falling-samurai-7438.herokuapp.com/twitter/callback", 
+  "HMAC-SHA1")
 
 app.configure(function() {
   app.use(express.static(__dirname + '/app'))
+  app.use(express.cookieParser(secret))
+  app.use(express.session({ secret : secret }))
 
   //views
   app.set('views', __dirname + '/jade')
@@ -27,19 +28,20 @@ app.get('/', function(req, res) {
 })
 
 app.get('/twitter/signin', function (req, res) {
-  consumer().getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
+  oa.getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
     if (error) {
-      res.send("Error getting OAuth request token : " + sys.inspect(error), 500);
+      console.log('error', error)
+      res.send("Error getting OAuth request token : " + error, 500);
     } else {  
       req.session.oauthRequestToken = oauthToken;
       req.session.oauthRequestTokenSecret = oauthTokenSecret;
-      res.redirect("https://api.twitter.com/1/oauth/authorize?oauth_token="+req.session.oauthRequestToken);      
+      res.redirect("https://api.twitter.com/oauth/authorize?oauth_token="+req.session.oauthRequestToken);      
     }
   })
 })
 
 app.get('/twitter/callback', function(req, res) {
-  consumer().getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
+  oa.getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
     if (error) {
       res.send("Error getting OAuth access token : " + sys.inspect(error) + "["+oauthAccessToken+"]"+ "["+oauthAccessTokenSecret+"]"+ "["+sys.inspect(results)+"]", 500)
     } else {
