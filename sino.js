07ded040e2ffa4b6092999a1205a2f9c46e02d4c
Bryan Maynard <bsgbryan@gmail.com>
2012-06-05 23:04:16 -0500

    Working on SiNO

--- sino.js
+++ sino.js
@@ -7,41 +7,36 @@ var creds   = 'http://twitter.com/account/verify_credentials.json',
     users   = 'https://api.twitter.com/1/users/search.json?q=',
     message = 'http://api.twitter.com/1/statuses/update.json'
 
-function SiNO(cb) {
-  return new OAuth(
-    'https://api.twitter.com/oauth/request_token', 
-    'https://api.twitter.com/oauth/access_token', 
-    process.env.TwitterConsumerKey,
-    process.env.TwitterConsumerSecret,
-    '1.0', 
-    cb, 
-    'HMAC-SHA1')
-}
+var oauth = new OAuth(
+  'https://api.twitter.com/oauth/request_token', 
+  'https://api.twitter.com/oauth/access_token', 
+  process.env.TwitterConsumerKey,
+  process.env.TwitterConsumerSecret,
+  '1.0', 
+  process.env.TwitterOAuthCallback, 
+  'HMAC-SHA1')
 
-SiNO.prototype.token = { 
+exports.token = { 
   request: function(res) {
-    oauth.getOAuthRequestToken(function(err, t, s, results) {
+    this.oauth.getOAuthRequestToken(function(err, t, s, results) {
 
       if (err) res.send(util.inspect(err), 500)
       else {
         this.token  = t
         this.secret = s
 
-        // We can't pass token here, not sure why
         res.redirect(auth + t)    
       }
     })
   },
   access: function(oauth_verifier, res) {
-    oauth.getOAuthAccessToken(
-      this.token, 
-      this.secret, 
-      oauth_verifier,
+
+    this.oauth.getOAuthAccessToken(this.token, this.secret, oauth_verifier,
 
       function(err, token, secret, results) {
         if (err) res.send(util.insepct(err), 500)
         else
-          oauth.get(creds, token, secret, function (error, data, response) {
+          this.oauth.get(creds, token, secret, function (error, data, response) {
             if (erro) res.send(util.insepct(erro), 500)
             else {
               res.cookie('twitter_user', JSON.parse(decodeURIComponent(data)).screen_name, { httpOnly: false, path: '/' })
@@ -52,9 +47,9 @@ SiNO.prototype.token = {
   }
 }
 
-SiNO.prototype.user = {
+exports.user = {
   search: function(name, success, error) {
-    oauth.get(users + name, process.env.TwitterAccessToken, process.env.TwitterAccessTokenSecret,
+    this.oauth.get(users + name, process.env.TwitterAccessToken, process.env.TwitterAccessTokenSecret,
     function (error, data, response) {
       if (error) res.send(util.inspect(error), 500)
       else res.send(data)
@@ -62,15 +57,13 @@ SiNO.prototype.user = {
   }
 }
 
-SiNO.prototype.statuses = {
+exports.statuses = {
   update: function(status, res) {
     // I have to pass the status parameter in the query string. Not sure why, but Twitter demands it.
-    oauth.post(message + '?status=' + encodeURIComponent(status), process.env.TwitterAccessToken, process.env.TwitterAccessTokenSecret, null, null,
+    this.oauth.post(message + '?status=' + encodeURIComponent(status), process.env.TwitterAccessToken, process.env.TwitterAccessTokenSecret, null, null,
       function (error, data, response) {
         if (error) res.send(util.inspect(error), 500)
         else res.send(data)
       })
   }
 }
-
-module.exports = SiNO
