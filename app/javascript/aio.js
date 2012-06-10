815ab601c2ffb81cfe736b43ba245e8264659026
Bryan Maynard <bsgbryan@gmail.com>
2012-06-10 15:23:18 -0500

    Tweaking things

--- app/javascript/aio.js
+++ app/javascript/aio.js
@@ -57,7 +57,7 @@
     if ($.cookie('AiOID') === null)
       $('#aio .sign.in').removeClass('hidden')
     else {
-      setInterval(getNewMessages, 3000)
+      setInterval(getNewMessages, 1000)
       $('#aio form.user.hidden').removeClass('hidden')
     }
 
