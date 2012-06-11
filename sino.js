fc2168064158ad2c51eb9886730581e3e570b518
Bryan Maynard <bsgbryan@gmail.com>
2012-06-11 01:19:22 -0500

    Trying to get streaming working

--- sino.js
+++ sino.js
@@ -2,7 +2,7 @@ var OAuth = require('bs-oauth').OAuth,
     util  = require('util'),
     sha1  = require('./app/sha1')
 
-var TwitterNode = require('./app/twitter-node/index').TwitterNode
+var TwitterNode = require('./app/twitter-node').TwitterNode
 
 // Twitter urls
 var creds   = 'http://twitter.com/account/verify_credentials.json',
