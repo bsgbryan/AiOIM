c5082c154bd7b299b690f66cb4ce3e9be3d5e32d
Bryan Maynard <bsgbryan@gmail.com>
2012-06-12 00:18:26 -0500

    Trying out these new fangled WebSockets

--- web.js
+++ web.js
@@ -39,13 +39,14 @@ app.get('/aio', function (req, res) {
 
     sockets[u] = io.
       of('/aio/' + u).
-      on('connection', function (data) {
-        console.log('CONNECTED USER', data)
-      }).
-      on('send message', function (message) {
-        var tweet = '@' + message.to + ' ' + message.content + ' #AiOIM'
-console.log('RECEIVED WEB SOCKET REQUEST FROM', u)
-        SiNO.statuses.update(tweet, u)
+      on('connection', function (socket) {
+        socket.on('send message', function (message) {
+          var tweet = '@' + message.to + ' ' + message.content + ' #AiOIM'
+          
+          console.log('RECEIVED WEB SOCKET REQUEST FROM', u)
+          
+          SiNO.statuses.update(tweet, u)
+        })console.log('CONNECTED USER', data)
       })
   }
 
