b02ae6c5d273189296da15c8030830ecd7634506
Bryan Maynard <bsgbryan@gmail.com>
2012-06-12 21:56:38 -0500

    Working

--- app/javascript/aio.js
+++ app/javascript/aio.js
@@ -96,7 +96,7 @@
         '</form>' +
       '</li>').removeClass('hidden')
 
-    $.getJSON('http://www.iheartquotes.com/api/v1/random?format=json&max_characters=140&callback=?', function (data) {
+    $.get('http://www.iheartquotes.com/api/v1/random?format=json&max_characters=140&callback=?', function (data) {
       console.log(data)
     }) 
   }
