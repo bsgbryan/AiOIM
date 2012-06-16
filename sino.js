7c54c4be7556a41373df496b45141e77756f3b7f
Bryan Maynard <bsgbryan@gmail.com>
2012-06-15 23:48:09 -0500

    Working with sessions

--- sino.js
+++ sino.js
@@ -35,13 +35,18 @@ exports.token = {
     oauth().getOAuthRequestToken(function (err, t, s, results) {
 
       if (err) res.send(util.inspect(err), 500)
-      else res.redirect(auth + t)
+      else {
+        req.session.token  = t
+        req.session.secret = s 
+              
+        res.redirect(auth + t)
+      }
     })
   },
   access: function(req, res) {
     var myauth = oauth()
 
-    myauth.getOAuthAccessToken(process.env.TwitterAccessToken, process.env.TwitterAccessTokenSecret, req.query.oauth_verifier,
+    myauth.getOAuthAccessToken(req.session.token, req.session.secret, req.query.oauth_verifier,
       function (err, token, secret, results) {
         
         if (err) res.send(util.inspect(err), 500)
