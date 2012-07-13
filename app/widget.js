0ea5628507495f830d3ccc0ca5e4c881a5c4b3ee
Bryan Maynard <bsgbryan@gmail.com>
2012-07-12 23:33:26 -0500

    Added a new message form to the widget

--- app/widget.js
+++ app/widget.js
@@ -24,44 +24,61 @@
   // Ganked from jquery.cookie
   function cookie(key, value, options) {
 
-      // key and at least value given, set cookie...
-      if (arguments.length > 1 && (!/Object/.test(Object.prototype.toString.call(value)) || value === null || value === undefined)) {
-          options = $.extend({}, options);
+    // key and at least value given, set cookie...
+    if (arguments.length > 1 && (!/Object/.test(Object.prototype.toString.call(value)) || value === null || value === undefined)) {
+        options = $.extend({}, options);
 
-          if (value === null || value === undefined) {
-              options.expires = -1;
-          }
+        if (value === null || value === undefined) {
+            options.expires = -1;
+        }
 
-          if (typeof options.expires === 'number') {
-              var days = options.expires, t = options.expires = new Date();
-              t.setDate(t.getDate() + days);
-          }
+        if (typeof options.expires === 'number') {
+            var days = options.expires, t = options.expires = new Date();
+            t.setDate(t.getDate() + days);
+        }
 
-          value = String(value);
+        value = String(value);
 
-          return (document.cookie = [
-              encodeURIComponent(key), '=', options.raw ? value : encodeURIComponent(value),
-              options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-              options.path    ? '; path=' + options.path : '',
-              options.domain  ? '; domain=' + options.domain : '',
-              options.secure  ? '; secure' : ''
-          ].join(''));
-      }
+        return (document.cookie = [
+            encodeURIComponent(key), '=', options.raw ? value : encodeURIComponent(value),
+            options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+            options.path    ? '; path=' + options.path : '',
+            options.domain  ? '; domain=' + options.domain : '',
+            options.secure  ? '; secure' : ''
+        ].join(''));
+    }
 
-      // key and possibly options given, get cookie...
-      options = value || {};
-      var decode = options.raw ? function(s) { return s; } : decodeURIComponent;
+    // key and possibly options given, get cookie...
+    options = value || {};
+    var decode = options.raw ? function(s) { return s; } : decodeURIComponent;
 
-      var pairs = document.cookie.split('; ');
-      for (var i = 0, pair; pair = pairs[i] && pairs[i].split('='); i++) {
-          if (decode(pair[0]) === key) return decode(pair[1] || ''); // IE saves cookies with empty string as "c; ", e.g. without "=" as opposed to EOMB, thus pair[1] may be undefined
-      }
-      return null;
+    var pairs = document.cookie.split('; ');
+    for (var i = 0, pair; pair = pairs[i] && pairs[i].split('='); i++) {
+        if (decode(pair[0]) === key) return decode(pair[1] || ''); // IE saves cookies with empty string as "c; ", e.g. without "=" as opposed to EOMB, thus pair[1] may be undefined
+    }
+    return null;
   };
+
+  function populateMessages() {
+
+  }
+
+  function showNewMessageForm() {
+    $('#aioim').append('<form action="http://aioim.bryanmaynard.com/statuses.update" method="post">' +
+        '<input type="hidden" name="AiOID" value="' + cookie('AiOID') + '">' +
+        '<textarea name="status" placeholder="What do you have to say?"></textarea>' +
+        '<button type="submit">say</button>' +
+      '</form>')
+  }
 
   function initialize() {
     if (cookie('AiOID') === null && typeof qParam('AiOID') === 'undefined')
       $('#aioim').prepend('<p><a class="authorize" href="https://aioim.bryanmaynard.com/signin">Sign in via Twitter</a> to join the discussion!</p>')
+    else {
+      populateMessages()
+      showNewMessageForm()
+    }
+
 
     if (typeof qParam('AiOID') === 'string')
       cookie('AiOID', qParam('AiOID'))
