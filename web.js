41601f0e5122c00178444a3ec632ed0fe4ff0b76
Bryan Maynard <bsgbryan@gmail.com>
2012-07-16 01:59:34 -0500

    Adding cookie back so AiOIM doesn't break

--- web.js
+++ web.js
@@ -51,7 +51,7 @@ var sock = {
   },
 
   error: function(error, code) {
-    console.log('twitter stream error "%@", "%s"', error, code)
+    console.log('twitter stream error', error, code)
   }
 }
 
