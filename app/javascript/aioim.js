23503e83b07fde79db9ed49fe4240eff38388013
Bryan Maynard <bsgbryan@gmail.com>
2012-07-26 22:37:21 -0500

    Forgot to wrap chat[message], oopsie

--- app/javascript/aioim.js
+++ app/javascript/aioim.js
@@ -81,10 +81,10 @@
         add   = false
 
     for (var message = 0; message < chat.length; message++)
-      if (chat[message].data('uid') < event.name())
+      if ($(chat[message]).data('uid') < event.name())
         add = true
       else
-        chat[message].after(
+        $(chat[message]).after(
           '<li class="' + person + '" id="' + mess.id_str + '" data-uid="' + event.name() + '">' +
           '<p class="message">' + mess.text.substring(mess.to.length + 2) + '</p>' +
           '</li>')
