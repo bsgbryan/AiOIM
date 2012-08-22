5cc69ba740b65e055770a0ad57c096dd5cfbf5aa
Bryan Maynard <bsgbryan@gmail.com>
2012-08-22 00:20:17 -0500

    Removed Redis as a dependency

--- web.js
+++ web.js
@@ -1,25 +1,16 @@
 var express = require('express'),
     SiNO    = require('./sino'),
     sha1    = require('./app/sha1'),
-    RedisStore = require('connect-redis')(express),
     sys        = require('util'),
     app        = express.createServer(express.logger()),
     hash       = 'Sta8aDaMaphubruhustEbr*pede7UbrePufR9cHU$uwup6U+udU&pekun5sp5@e?',
     http       = require('http')
-
-// Production
-if (process.env.REDISTOGO_URL) 
-  var redis = require('redis-url').connect(process.env.REDISTOGO_URL)
-// Development
-else 
-  var redis = require('redis').createClient()
 
 app.configure(function() {
   app.use(express.static(__dirname + '/app'))
   app.use(express.bodyParser())
   app.use(express.cookieParser(hash))
   app.use(express.session({ 
-    store  : new RedisStore({ client : redis }),
     secret : hash,
     cookie : {
       maxAge : 1209600000,
